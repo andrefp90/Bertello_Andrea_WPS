d26d959f5bf2077f7de8149b2d9f53fba2c6f572
andrea <andrefp90@fullsail.edu>
2015-05-07 17:51:08 -0500

    week 1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	$('.nav_menu .dropdown').css('display','block');
+	$('.nav_menu > ul').dropmenu({
+		effect : 'slide',
+		speed : 250,
+		timeout : 0,
+		nbsp : false
+		});
+		
+		// Indicate items with sub-menu
+	$('li').has('ul').find('> a').addClass('indicator');
+	
+	});
+	
+
+
+ 
+
